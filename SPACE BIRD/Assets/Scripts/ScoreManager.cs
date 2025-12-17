d130180cb9fe4c226412fc25bdb77bfee1cf5302
TakeruTeraoka <teraokatakeru@gmail.com>
2025-12-17 22:30:46 +0900

    ScoreManagerの追加

---
+++ SPACE BIRD/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int totalScore = 0;
+    public static int addScore = 0;
+    private int score = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.GetComponent<Text>().text = score.ToString("000000");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (addScore != 0)
+        {
+            score += addScore;
+            totalScore += addScore;
+            if (score > 999999)
+            {
+                //écã@ÇàÍÇ¬ëùÇ‚Ç∑
+                score = 0;
+            }
+            this.GetComponent<Text>().text = score.ToString("000000");
+            addScore = 0;
+        }
+
+        Debug.Log("TotalScore:" + totalScore);
+    }
+}
